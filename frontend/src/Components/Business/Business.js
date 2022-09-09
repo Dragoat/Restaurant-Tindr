48d9f24cb1a45260506da6d82d00161a1b3ca7e8
Paige <paigehall006@gmail.com>
2022-09-09 17:28:37 -0400

    hours/days

--- frontend/src/Components/Business/Business.js
+++ frontend/src/Components/Business/Business.js
@@ -11,6 +11,19 @@ function Business (props) {
   const [showComponent, setShowComponent] = useState(false);
   const [mondayStart, setMondayStart] = useState('');
   const [mondayEnd, setMondayEnd] = useState('');
+  const [tuesdayStart, setTuesDayStart] = useState('');
+  const [tuesdayEnd, setTuesdayEnd] = useState('');
+  const [wednesdayStart, setWednesdayStart] = useState('');
+  const [wesnesdayEnd, setWednesdayEnd] = useState('');
+  const [thursdayStart, setThursdayStart] = useState('');
+  const [thursdayEnd, setThursdayEnd] = useState('');
+  const [fridayStart, setFridayStart] = useState('');
+  const [fridayEnd, setFridayEnd] = useState('');
+  const [saturdayStart, setSaturdayStart] = useState('');
+  const [saturdayEnd, setSaturdayEnd] = useState('');
+  const [sundayStart, setSundayStart] = useState('');
+  const [sundayEnd, setSundayEnd] = useState('');
+
 
   const handleClick = async() => {
     setShowComponent(true);
@@ -20,16 +33,108 @@ function Business (props) {
     }
     }).then(res => {
     console.log(res.data)
-    setMondayStart(changeTime(res.data.hours[0].open[0].start))
-    setMondayEnd(changeTime(res.data.hours[0].open[0].end))
+
+    if(!res.data.hours){
+    } else {
+      if(res.data.hours[0].open[0]) {
+        if(res.data.hours[0].open[0].start === undefined){
+          setMondayStart('Closed')
+          setMondayEnd('Closed')
+        } else { 
+        setMondayStart(changeTime(res.data.hours[0].open[0].start))
+        setMondayEnd(changeTime(res.data.hours[0].open[0].end))
+      }
+    } else {
+      setMondayStart('N/A')
+      setMondayEnd('N/A')
+    }
+
+      if(res.data.hours[0].open[1]) {
+        if(res.data.hours[0].open[1].start === undefined){
+          setTuesDayStart('Closed')
+          setTuesdayEnd('Closed')
+        } else {
+        setTuesDayStart(changeTime(res.data.hours[0].open[1].start))
+        setTuesdayEnd(changeTime(res.data.hours[0].open[1].end))
+        }
+      } else {
+        setTuesDayStart('N/A')
+        setTuesdayEnd('N/A')
+      }
+
+        if(res.data.hours[0].open[2]) {
+        if(res.data.hours[0].open[2].start === undefined){
+          setWednesdayStart('Closed')
+          setWednesdayEnd('Closed')
+        } else {
+        setWednesdayStart(changeTime(res.data.hours[0].open[2].start))
+        setWednesdayEnd(changeTime(res.data.hours[0].open[2].end))
+        }
+      } else {
+        setWednesdayStart('N/A')
+        setWednesdayEnd('N/A')
+      }
+
+        if(res.data.hours[0].open[3]) {
+        if(res.data.hours[0].open[3].start === undefined){
+          setThursdayStart('Closed')
+          setThursdayEnd('Closed')
+        } else {
+        setThursdayStart(changeTime(res.data.hours[0].open[3].start))
+        setThursdayEnd(changeTime(res.data.hours[0].open[3].end))
+        }
+      } else {
+        setThursdayStart('N/A')
+        setThursdayEnd('N/A')
+      }
+
+        if(res.data.hours[0].open[4]) {  
+        if(res.data.hours[0].open[4].start === undefined){
+          setFridayStart('Closed')
+          setFridayEnd('Closed')
+        } else {
+        setFridayStart(changeTime(res.data.hours[0].open[4].start))
+        setFridayEnd(changeTime(res.data.hours[0].open[4].end))
+        }
+      } else {
+        setFridayStart('N/A')
+        setFridayEnd('N/A')
+      }
+
+        if(res.data.hours[0].open[5]) {
+        if(res.data.hours[0].open[5] === undefined){
+          setSaturdayStart('Closed')
+          setSaturdayEnd('Closed')
+        } else {
+        setSaturdayStart(changeTime(res.data.hours[0].open[5].start))
+        setSaturdayEnd(changeTime(res.data.hours[0].open[5].end))
+        }
+      } else {
+        setSaturdayStart('N/A')
+        setSaturdayEnd('N/A')
+      }
+
+        if(res.data.hours[0].open[6]) {
+        if(res.data.hours[0].open[6] === undefined){
+          setSundayStart('Closed') 
+          setSundayEnd('Closed')
+        } else {
+        setSundayStart(changeTime(res.data.hours[0].open[6].start))
+        setSundayEnd(changeTime(res.data.hours[0].open[6].end))
+        }
+      } else {
+        setSundayStart('N/A')
+        setSundayEnd('N/A')
+      }
+    }
     })
   }
 
 
 
-  function changeTime(startTime) {
-    const hours = startTime.slice(0, 2);
-    const minutes = startTime.slice(-2);
+  function changeTime(time) {
+    const hours = time.slice(0, 2);
+    const minutes = time.slice(-2);
     const AmOrPm = hours >= 12 ? 'pm' : 'am';
     const finalTime = hours % 12 || 12;
     return finalTime + ':' + minutes + AmOrPm;
@@ -41,7 +146,10 @@ function Business (props) {
         <h2>{props.business.name}</h2>
         <p>{props.business.id}</p>
         <button onClick={handleClick}>View More Details</button>
-        {showComponent ? <BusinessInfo startTime={mondayStart} closeTime={mondayEnd}/> : null} 
+        {showComponent ? <BusinessInfo monStart={mondayStart} monClose={mondayEnd} 
+        tuesStart={tuesdayStart} tuesClose={tuesdayEnd} wedStart={wednesdayStart} wedClose={wesnesdayEnd} 
+        thursStart={thursdayStart} thursClose={thursdayEnd} friStart={fridayStart} friClose={fridayEnd} 
+        satStart={saturdayStart} satClose={saturdayEnd} sunStart={sundayStart} sunClose={sundayEnd}/> : null} 
         <p>{props.business.transactions[0]} {props.business.transactions[1]} {props.business.transactions[2]}</p>
         <p>{props.business.isClosed}</p>
         <a href = {`https://maps.google.com/?q=${props.business.latitude},${props.business.longitude}`}>View on Google Maps</a>
--- frontend/src/Components/BusinessInfo/BusinessInfo.js
+++ frontend/src/Components/BusinessInfo/BusinessInfo.js
@@ -5,7 +5,13 @@ function BusinessInfo(props) {
     return ( 
         <div>
         <h1>Hours</h1>
-        <p>Monday: {props.startTime} -{props.closeTime}</p>
+        <p>Monday: {props.monStart} -{props.monClose}</p>
+        <p>Tuesday: {props.tuesStart} - {props.tuesClose} </p>
+        <p>Wednesday: {props.wedStart} - {props.wedClose}</p>
+        <p>Thursday: {props.thursStart} - {props.thursClose}</p>
+        <p>Friday: {props.friStart} - {props.friClose}</p>
+        <p>Saturday: {props.satStart} - {props.satClose}</p>
+        <p>Sunday: {props.sunStart} - {props.sunClose}</p>
         </div>
      );
 }
